9686cd780c4e7fcf55e3246d956ed9e5eb39cdc7
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-17 16:50:10 +0100

    primo commit

---
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -0,0 +1,21 @@
+package org.generation.italy.array;
+
+		public class CibiPreferiti {
+			public static void main(String[] args) {
+				
+				String[] cibiPreferiti = {"Pizza", "Brasato e polenta", "Crispy McBacon", "Tagliata di tonno", "Pasta al Ragù", "Lasagne", "Carpaccio", "Ciambelle", "Biscotti", "Gelato"};
+				int lunghezzaArray = cibiPreferiti.length;
+				
+				System.out.println(lunghezzaArray);
+				System.out.println("Il mio cibo top è: " + cibiPreferiti[9]);
+				System.out.println("Il mio cibo preferito ma non troppo è: " + cibiPreferiti[8]);
+				
+				System.out.println("Il cibo di mezza classifica è: " + cibiPreferiti[(cibiPreferiti.length-1) / 2]);
+			}
+
+		}
+
+
+	
+
+
